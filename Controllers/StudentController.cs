0177bacedd4950e8ff0bd55e23282877a3a1a87b
ArijKouki <arijkouki17@gmail.com>
2022-12-16 21:40:05 +0100

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication4.Models;
+using WebApplication4.Data;
+
+namespace WebApplication4.Controllers
+{
+	public class StudentController : Controller
+	{
+		private readonly ILogger<StudentController> _logger;
+
+		public StudentController(ILogger<StudentController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			
+			/*Debug.WriteLine("Here" + UniversityContext.Instance);
+			Debug.WriteLine("Here" + UniversityContext.Instance);
+
+			List<Student> s = UniversityContext.Instance.Student.ToList();
+			Debug.WriteLine(s.Count);*/
+
+			StudentRepository studentRepository = new StudentRepository();
+			List<Student> l = studentRepository.all();
+			ViewData["studentsList"] = l;
+
+			return View();
+		}
+
+		public IActionResult Courses()
+		{
+            StudentRepository studentRepository = new StudentRepository();
+			List<string> l = studentRepository.UniqueCourses();
+            ViewData["CoursesList"] = l;
+			return View();
+		}
+
+        public IActionResult ByCourse(string course)
+        {
+            StudentRepository studentRepository = new StudentRepository();
+            List<Student> l = studentRepository.StudentsByCourse(course);
+			ViewData["course"] = course;
+            ViewData["StudentsList"] = l;
+            return View();
+        }
+
+        public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Data/UniversityContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+	public class UniversityContext : DbContext
+	{
+		private static UniversityContext? _instance;
+		public DbSet<Student>? Student { get; set; }
+		public static UniversityContext Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = Instantiate_University_Context();
+				}
+				return _instance;
+			}
+		}
+
+		public UniversityContext(DbContextOptions o) : base(o) { }
+
+		public static UniversityContext Instantiate_University_Context()
+		{
+			Debug.WriteLine("Creating a new University Context");
+			var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+
+
+			var connection = optionsBuilder.UseSqlite(@"Data Source=C:\\Users\\MSI\\source\\repos\\WebApplication4\\TP4.db");
+			return new UniversityContext(optionsBuilder.Options);
+		}
+
+
+	}
+}
